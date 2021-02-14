5c76dcea09cf8eb71107223a16afce93f1016947
jjaykim <dnglglmm@gmail.com>
2021-02-14 17:41:16 -0500

    Week 4

---
+++ WS04/DIY/Label.h
@@ -0,0 +1,44 @@
+//==============================================
+// Name:           Jungjoo Kim
+// Student Number: 162 641 195
+// Email:          jkim594@myseneca.ca
+// Section:        NCC
+// Workshop:       Workshop4_DIY
+//==============================================
+// Label.h
+
+#ifndef SDDS_LABEL_H_
+#define SDDS_LABEL_H_
+
+using namespace std;
+
+namespace sdds {
+  const char defaultSymbol[9] = "+-+|+-+|";
+
+  class Label
+  {
+    char symbol[9];
+    char *oneLineText;
+
+  public:
+    // Creates an empty label and defaults the frame to defaultSymbol
+    Label();
+
+    // Creates an empty label and sets the frame to the frameArg argument
+    Label(const char *frameArg);
+
+    // Creates a Label with the frame set to frameArg
+    // and the content of the Label set to the corresponding argument
+    Label(const char *frmaeArg, const char *content);
+
+    // Makes sure there is no memory leak when the label goes out of scope
+    ~Label();
+
+    // Reads the label from console up to 70 characters and stores it in the Label
+    void readLabel();
+
+    // Prints the label by drawing the frame around the content
+    std::ostream &printLabel() const;
+  };
+}
+#endif // !SDDS_LABEL_H_
