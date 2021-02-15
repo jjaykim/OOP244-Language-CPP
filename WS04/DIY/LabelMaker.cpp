8f179d8e2b3cd0883a40a3ad03e4452cd2acfb85
jjaykim <dnglglmm@gmail.com>
2021-02-14 20:47:02 -0500

    Week 4

--- WS04/DIY/LabelMaker.cpp
+++ WS04/DIY/LabelMaker.cpp
@@ -15,10 +15,16 @@ using namespace std;
 namespace sdds {
   LabelMaker::LabelMaker(int noOfLabels)
   {
-    totalLabels = noOfLabels;
+    if (noOfLabels > 0) {
+      totalLabels = noOfLabels;
 
-    // creates a dynamic array of Labels to the size of noOfLabels
-    listOfLabels = new Label[noOfLabels];
+      // creates a dynamic array of Labels to the size of noOfLabels
+      listOfLabels = new Label[noOfLabels];
+    }
+    else {
+      cout << "You can only input greater than zero" << endl;
+      noOfLabels = 0;
+    }
   }
 
   LabelMaker::~LabelMaker()
@@ -30,11 +36,13 @@ namespace sdds {
 
   void LabelMaker::readLabels()
   {
-    cout << "Enter " << totalLabels << " labels:" << endl;
+    if (totalLabels > 0) {
+      cout << "Enter " << totalLabels << " labels:" << endl;
 
-    for (int i = 0; i < totalLabels; i++) {
-      cout << "Enter label number " << i + 1 << endl;
-      listOfLabels[i].readLabel();
+      for (int i = 0; i < totalLabels; i++) {
+        cout << "Enter label number " << i + 1 << endl;
+        listOfLabels[i].readLabel();
+      }
     }
   }
 
