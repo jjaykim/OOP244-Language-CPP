477fe2c7facf02af0a68d6d37a0aff930b4a3a0a
jjaykim <dnglglmm@gmail.com>
2021-03-23 20:09:35 -0400

    Milestone 1 - utils & Time module

--- Milestone/MS1/src/utils.h
+++ Milestone/MS1/src/utils.h
@@ -1,11 +1,26 @@
-//==============================================
-// Name:           Jungjoo Kim
-// Student Number: 162 641 195
-// Email:          jkim594@myseneca.ca
-// Section:        NCC
-// Final Project Milestone 1
-//==============================================
-// utils.h
+/*
+ < Citation and Sources >
+
+Final Project Milestone 1
+Module: Utils
+Filename: utils.h
+Version 1.0
+
+Author:	        Jungjoo Kim
+Student Number: 162 641 195
+Email:          jkim594@myseneca.ca
+Section:        NCC
+
+Revision History
+-----------------------------------------------------------
+Date       Reason
+2021/3/13  First crating coding
+2021/3/14  Completed coding
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------
+*/
 
 #ifndef SDDS_UTILS_H_
 #define SDDS_UTILS_H_
